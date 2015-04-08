737bacac5ff65d036a5b9fc0e574c1308a972eb5
CC007 <Coolcat_the_best2@hotmail.com>
2015-04-08 13:11:59 +0200

    added rabbitmq extra comment

--- startDemo.bat
+++ startDemo.bat
@@ -1,9 +1,9 @@
 @REM variables: set to correct values
-@set mavenLocation=C:\Program Files\NetBeans 8.0.1\java\maven\bin\mvn.bat
-@set projectLocation=C:\Users\Rik\Documents\NetBeansProjects\Netcomputing\LightSaverMaven
-@set demoLocation=C:\Users\Rik\Documents\NetBeansProjects\LightSaverDemo
-@set rmiLocation=C:\Program Files\Java\jdk1.8.0_31\jre\bin\rmiregistry.exe
-@set rmqLocation=C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.4\sbin\rabbitmq-server.bat
+@set mavenLocation=P:\Program Files\NetBeans 8.0.2\java\maven\bin\mvn.bat
+@set projectLocation=D:\Local\My Documents\NetBeansProjects\LightSaverMaven
+@set demoLocation=D:\Local\My Documents\NetBeansProjects\LightSaverDemo
+@set rmiLocation=P:\Program Files\Java\jdk1.8.0\jre\bin\rmiregistry.exe
+@set rmqLocation=P:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.0\sbin\rabbitmq-server.bat
 
 @REM set base string for executing project files
 @set runStr=start cmd.exe /k ""%mavenLocation%" exec:java -Dexec.mainClass="com.github.cc007.lightsaver.
@@ -13,7 +13,9 @@ cd "%projectLocation%"
 
 @REM run rmi registry
 start cmd.exe /k ""%rmiLocation%""
-start cmd.exe /k ""%rmqLocation%"" start
+
+@REM enable running RabbitMQ if needed
+REM start cmd.exe /k ""%rmqLocation%"" start
 
 ping 127.0.0.1 -n 5 -w 1000 > NUL
 
