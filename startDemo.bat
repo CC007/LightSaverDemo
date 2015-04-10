f6061e0a96dae1c9dbba06020cf40d44c31b9a74
CC007 <Coolcat_the_best2@hotmail.com>
2015-04-10 06:51:19 +0200

    Added cd to right location to start rmi

--- startDemo.bat
+++ startDemo.bat
@@ -8,20 +8,24 @@
 @REM set base string for executing project files
 @set runStr=start cmd.exe /k ""%mavenLocation%" exec:java -Dexec.mainClass="com.github.cc007.lightsaver.
 
-@REM Go to project folder
-cd "%projectLocation%"
+@REM Go to project bin folder
+cd "%projectLocation%\target\classes"
 
 @REM run rmi registry
 start cmd.exe /k ""%rmiLocation%""
 
+@REM Go to project folder
+cd "%projectLocation%"
+
 @REM enable running RabbitMQ if needed
 REM start cmd.exe /k ""%rmqLocation%"" start
 
-ping 127.0.0.1 -n 5 -w 1000 > NUL
+@echo "wait 5 seconds"
+@ping 127.0.0.1 -n 5 -w 1000 > NUL
 
 @REM Run java programs
 %runStr%datacontroller.DataController""
-%runStr%appliance.light.Light" -Dexec.args="1 localhost""
+%runStr%appliance.light.Light" -Dexec.args="2 localhost""
 %runStr%detector.light.LightDetector" -Dexec.args="1 localhost""
 %runStr%detector.motion.MotionDetector" -Dexec.args="1 localhost""
 %runStr%detector.passage.PassageDetector" -Dexec.args="1 localhost""
